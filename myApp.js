17aae311f67a698a0860e5228cc7e88545c6156b
Christopher Forti <christopher.forti.79@gmail.com>
2024-08-20 19:39:53 -0400

    Started connection with mongDB.

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 // Connect to MongoDB using mongoose
 mongoose
-  .connect("your_mongodb_atlas_uri", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
